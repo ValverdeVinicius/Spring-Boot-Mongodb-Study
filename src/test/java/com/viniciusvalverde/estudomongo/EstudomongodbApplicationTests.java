b077d64213c1310ff6107fbbf22fc66e9694c940
Vinicius Valverde <valverdevinicius@protonmail.com>
2023-07-03 14:37:53 -0300

    Creating User and his Endpoint

---
+++ src/test/java/com/viniciusvalverde/estudomongo/EstudomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusvalverde.estudomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
